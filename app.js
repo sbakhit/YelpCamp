9ae2374a65ffc30f923ddaba09b65c023a925ac8
sbakhit <saleh.bakhit@hotmail.com>
2020-01-22 19:36:58 -0500

    v6-add auth to comments

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
-const express    = require('express'),
-      app        = express();
-      bodyParser = require('body-parser'),
-      mongoose   = require('mongoose'),
-      Campground = require('./models/campground'),
-      Comment    = require('./models/comment'),
-      seedDB     = require('./models/seeds'),
-      PORT       = 3000;
+const express               = require('express'),
+      app                   = express();
+      bodyParser            = require('body-parser'),
+      mongoose              = require('mongoose'),
+      passport              = require('passport'),
+      LocalStrategy         = require('passport-local'),
+      passportLocalMongoose = require('passport-local-mongoose'),
+      User                  = require('./models/user'),
+      Campground            = require('./models/campground'),
+      Comment               = require('./models/comment'),
+      seedDB                = require('./models/seeds'),
+      PORT                  = 3000;
 
 // app setup
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + "/public"));
+
+// passport setup
+app.use(require('express-session')({
+    secret: "secret word",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    next();
+});
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // mongoose setup
 mongoose.set('useFindAndModify', false);
@@ -65,7 +85,7 @@ app.get("/campgrounds/:id", (req, res) => {
 });
 
 //CREATE - add comment to campground
-app.post("/campgrounds/:id/comments", (req, res) => {
+app.post("/campgrounds/:id/comments", isLoggedIn, (req, res) => {
     Campground.findById(req.params.id, (err, campground) => {
         if(err) {
             console.log(`Error:\n${err}`);
@@ -85,7 +105,7 @@ app.post("/campgrounds/:id/comments", (req, res) => {
 });
 
 //NEW - show form to create new comment to campground
-app.get("/campgrounds/:id/comments/new", (req, res) => {
+app.get("/campgrounds/:id/comments/new", isLoggedIn, (req, res) => {
     Campground.findById(req.params.id, (err, campground) => {
         if(err) {
             console.log(`Error:\n${err}`);
@@ -94,6 +114,49 @@ app.get("/campgrounds/:id/comments/new", (req, res) => {
         }
     });
 });
+
+// auth routs
+app.get("/register", (req, res) => {
+    res.status(200).render("register");
+});
+
+app.post("/register", (req, res) => {
+    const newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, (err, user) => {
+        if(err) {
+            console.log(`Error:\n${err}`);
+            res.render("register");
+        } else {
+            passport.authenticate("local")(req, res, () => {
+                res.redirect("/campgrounds");
+            });
+        }
+    });
+});
+
+app.get("/login", (req, res) => {
+    res.status(200).render("login");
+});
+
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/campgrounds",
+    failureRedirect: "/login"
+}), (req, res) => {
+});
+
+
+app.get("/logout", (req, res) => {
+    req.logout();
+    res.status(200).redirect("/campgrounds");
+});
+
+function isLoggedIn(req, res, next) {
+    if(req.isAuthenticated()) {
+        next();
+    } else {
+        res.redirect("/login");
+    }
+}
 
 app.listen(PORT, () => {
     console.log(`Server is up on port ${PORT}.`);
